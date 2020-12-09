947ae2625327919ed37ffcf9f1bb4e8056554c7f
cynthiaQ40J05 <71242558+cynthiaQ40J05@users.noreply.github.com>
2020-12-08 19:22:39 -0600

    sendtcp powershell

---
+++ PIA_CIBERSEG/SendTCP.ps1
@@ -0,0 +1,34 @@
+﻿Param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [ValidateNotNullOrEmpty()]
+    [string]$EndPoint
+    , 
+    [Parameter(Mandatory=$true, Position=1)]
+    [ValidateNotNullOrEmpty()]
+    [int]$Port
+    , 
+    [Parameter(Mandatory=$true, Position=2)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Message
+) 
+
+Process{
+    # Setup connection 
+    $IP = [System.Net.Dns]::GetHostAddresses($EndPoint) 
+    $Address = [System.Net.IPAddress]::Parse($IP) 
+    $Socket = New-Object System.Net.Sockets.TCPClient($Address,$Port) 
+    
+    # Setup stream wrtier 
+    $Stream = $Socket.GetStream() 
+    $Writer = New-Object System.IO.StreamWriter($Stream)
+    
+    # Write message to stream
+    $Message | % {
+        $Writer.WriteLine($_)
+        $Writer.Flush()
+    }
+    
+    # Close connection and stream
+    $Stream.Close()
+    $Socket.Close()
+}
